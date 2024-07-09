e1110f073d34c8cdca42d277fba1555bc59b02eb
adilrhyder <adilrhyder@gosaas.io>
2024-07-09 11:06:06 +0500

    Refactor - simplify game query

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,14 @@ import NavBar from "./components/NavBar"
 import GameGrid from "./components/GameGrid"
 import GenreList from "./components/GenreList"
 import { useState } from "react"
-import { Genre } from "./hooks/useGenres"
 import PlatformSelector from "./components/PlatformSelector"
 import { Platform } from "./hooks/usePlatforms"
 import SortSelector from "./components/SortSelector"
 import GameHeading from "./components/GameHeading"
 
 export interface GameQuery {
-    genre: Genre | null
-    platform: Platform | null
+    genreId?: number
+    platformId?: number
     sortOrder: string
     searchText: string
 }
@@ -41,9 +40,9 @@ function App() {
                 <GridItem area="aside" paddingX={5}>
                     <GenreList
                         onSelectGenre={(genre) =>
-                            setGameQuery({ ...gameQuery, genre })
+                            setGameQuery({ ...gameQuery, genreId: genre.id })
                         }
-                        selectedGenre={gameQuery.genre}
+                        selectedGenreId={gameQuery.genreId}
                     />
                 </GridItem>
             </Show>
@@ -53,9 +52,12 @@ function App() {
                     <Flex marginBottom={5}>
                         <Box marginRight={5}>
                             <PlatformSelector
-                                selectedPlatform={gameQuery.platform}
+                                selectedPlatformId={gameQuery.platformId}
                                 onSelectPlatform={(platform) =>
-                                    setGameQuery({ ...gameQuery, platform })
+                                    setGameQuery({
+                                        ...gameQuery,
+                                        platformId: platform.id,
+                                    })
                                 }
                             />
                         </Box>
