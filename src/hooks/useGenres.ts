180af87417ea5172f1ec91007c8cce191d16969a
adilrhyder <adilrhyder@gosaas.io>
2024-07-09 10:05:58 +0500

    Refactoring - fetch games with react query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,6 @@
 // import useData from "./useData"
+import { useQuery } from "@tanstack/react-query"
+import apiClient, { FetchResponse } from "../services/api-client"
 import genres from "../data/genres"
 
 export interface Genre {
@@ -8,6 +10,15 @@ export interface Genre {
 }
 
 // const useGenres = () => useData<Genre>("/genres")
-const useGenres = () => ({ data: genres, isLoading: false, error: null })
+const useGenres = () =>
+    useQuery({
+        queryKey: ["genres"],
+        queryFn: () =>
+            apiClient
+                .get<FetchResponse<Genre>>("/genres")
+                .then((res) => res.data),
+        staleTime: 24 * 60 * 60 * 1000, // 24hr
+        initialData: { count: genres.length, results: genres },
+    })
 
 export default useGenres
