e84affddb7cc541ae1a45e9908a74b8b22c3cca0
adilrhyder <adilrhyder@gosaas.io>
2024-07-09 11:23:43 +0500

    Refactor - create lookup hooks

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -15,9 +15,9 @@ export interface Genre {
 const useGenres = () =>
     useQuery({
         queryKey: ["genres"],
-        queryFn: apiClient.getAll, 
+        queryFn: apiClient.getAll,
         staleTime: 24 * 60 * 60 * 1000, // 24hr
-        initialData: { count: genres.length, results: genres },
+        initialData: genres,
     })
 
 export default useGenres
