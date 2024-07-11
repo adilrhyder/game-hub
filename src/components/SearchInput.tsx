6b10e460ce590965d31a1cb7070baf14ad89fa5f
adilrhyder <adilrhyder@gosaas.io>
2024-07-11 13:42:30 +0500

    Fix the navbar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,18 +1,22 @@
-import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react"
-import { useRef } from "react"
-import { BsSearch } from "react-icons/bs"
-import useGameQueryStore from "../store"
-
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useRef } from "react";
+import { BsSearch } from "react-icons/bs";
+import useGameQueryStore from "../store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
-    const ref = useRef<HTMLInputElement>(null)
-    const setSearchText = useGameQueryStore(s => s.setSearchText)
+    const ref = useRef<HTMLInputElement>(null);
+    const setSearchText = useGameQueryStore((s) => s.setSearchText);
+    const navigate = useNavigate();
 
     return (
         <form
             onSubmit={(event) => {
-                event.preventDefault() //prevent form from being posted to the server
-                if (ref.current) setSearchText(ref.current.value)
+                event.preventDefault(); //prevent form from being posted to the server
+                if (ref.current) {
+                    setSearchText(ref.current.value);
+                    navigate("/");
+                }
             }}
         >
             <InputGroup>
@@ -25,7 +29,7 @@ const SearchInput = () => {
                 />
             </InputGroup>
         </form>
-    )
-}
+    );
+};
 
-export default SearchInput
+export default SearchInput;
