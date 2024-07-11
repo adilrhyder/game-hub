721ad257c6b8088c49564146473dae3c5d73ac54
adilrhyder <adilrhyder@gosaas.io>
2024-07-11 13:51:17 +0500

    Refactor entity interfaces

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,8 +1,8 @@
-import { Genre } from "./Genre";
-import { Platform } from "./Platform";
-import { Publisher } from "./Plublisher";
+import Genre from "./Genre";
+import Platform from "./Platform";
+import Publisher from "./Plublisher";
 
-export interface Game {
+export default interface Game {
     description_raw: string;
     genres: Genre[];
     publishers: Publisher[];
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,9 +1,8 @@
-// import useData from "./useData"
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
 import genres from "../data/genres";
 import ms from "ms";
-import { Genre } from "../entities/Genre";
+import Genre from "../entities/Genre";
 
 const apiClient = new APIClient<Genre>("/genres");
 
